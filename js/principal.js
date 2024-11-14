c0bf322f7b119c0ce133b0c9569017771ed4156c
kauephe <165967721+kauephe@users.noreply.github.com>
2024-11-14 01:36:37 +0000

    Initial commit

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+var titulo = document.querySelector('h1');
+titulo.textContent ="kaue nutrição";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "pacientes cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var nomeTd =paciente.querySelector(".info-nome");
+var pesoTd =paciente.querySelector(".info-peso");
+var alturaTd =paciente.querySelector(".info-altura");
+  var gorduraTd =paciente.querySelector(".info-gordura");
